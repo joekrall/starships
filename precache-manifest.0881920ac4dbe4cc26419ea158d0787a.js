990d85000f9c8b8c1c25bf9b7693ebd0e1bd247a
Joe Krall <kralljr@outlook.com>
2020-08-24 12:07:27 -0400

    Updates

---
+++ precache-manifest.0881920ac4dbe4cc26419ea158d0787a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76f319e6bd9caba50f46befe592bce59",
+    "url": "/starships/index.html"
+  },
+  {
+    "revision": "2f155cad76486f545f50",
+    "url": "/starships/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "a47085f15ad446b5e5b2",
+    "url": "/starships/static/css/main.0802758c.chunk.css"
+  },
+  {
+    "revision": "2f155cad76486f545f50",
+    "url": "/starships/static/js/2.5c70d346.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/starships/static/js/2.5c70d346.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a47085f15ad446b5e5b2",
+    "url": "/starships/static/js/main.e8b17ceb.chunk.js"
+  },
+  {
+    "revision": "b98beb907e10a25fb4c4",
+    "url": "/starships/static/js/runtime-main.4eb0037b.js"
+  }
+]);
